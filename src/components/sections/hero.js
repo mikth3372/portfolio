8f038401ce87a4b33a37133d748e02a6e59577c8
Mikhael Zacharias Thomas <143560798+mikth3372@users.noreply.github.com>
2025-01-23 07:47:17 +0530

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -61,7 +61,7 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Mikhael Zacharias Thomas.</h2>;
-  const three = <h3 className="big-heading">I build softwares for fun.</h3>;
+  const three = <h3 className="big-heading">I build software for fun.</h3>;
   // const four = (
   //   <>
   //     <p>
